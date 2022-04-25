7085ae757d965b3d4daf7b0383536f79a4737e89
IvanJust <deck2014@mail.ru>
2022-04-25 19:05:04 +0300

    master

--- vop.js
+++ vop.js
@@ -1,4 +1,20 @@
 const items = ["items", "apple", "items", "orange", "items", "apple"];
-function finditem(item){
-
+let mo = items[0];
+    let n = 0;
+function FindItem(item){
+    let map = {};
+    for(var i = 0; i < items.length; i++){
+        if(!map[items[i]]){
+            map[items[i]]=1;
+        }else{
+            ++map[items[i]];
+            console.log(map[items[i]])
+            if(map[items[i]]>map[mo]){
+                mo = items[i];
+            }
+        }
+    }
+    n = map[mo];
+    return mo;
 }
+console.log(`Элемент ${FindItem(items)} встречается ${n} раз(а)`)
